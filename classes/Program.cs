8fee0bd4cebb9f9fdeb79ec25128591cefa2a6c5
ilya1 <ilya1@DESKTOP-R4A653G>
2022-10-21 20:40:13 +0300

    Добавьте файлы проекта.

---
+++ classes/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    internal class Program
+    {
+        class Student
+        {
+
+            public string Name;
+            public string MidleName;
+            public string LastName;
+            public double estimation1;
+            public double estimation2;
+            public double SrEstimation;
+
+        }
+
+        class Programm
+        {
+            static Student GetStudent()
+            {
+                Student student = new Student();
+                student.Name = " Иван";
+                student.MidleName = " Иваночич";
+                student.LastName = "Иванов";
+                student.estimation1 = 3;
+                student.estimation2 = 5;
+                student.SrEstimation = (student.estimation1 + student.estimation2) / 2;
+
+                return student;
+
+            }
+            static void Print(Student student)
+            {
+                Console.WriteLine("Информация о студенте");
+                Console.WriteLine($"Фамилия:{student.LastName}");
+                Console.WriteLine($"Имя:{student.Name}");
+                Console.WriteLine($"Отчество:{student.MidleName}");
+                Console.WriteLine($"Оценка 1: {student.estimation1}");
+                Console.WriteLine($"Оценка 1: {student.estimation2}");
+                Console.WriteLine($"Средняя оценка: {student.SrEstimation}");
+            }
+            static void Main(string[] args)
+            {
+                var firstStudent = GetStudent();
+                Print(firstStudent);
+            }
+        }
+    }
+}
+
